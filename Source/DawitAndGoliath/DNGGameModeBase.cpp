3e28cc4aa662c6b74444f49f61df362e3de26e76
SeungGeol Song <unengine@gmail.com>
2019-06-04 09:22:55 +0900

    add gamemode file

--- Source/DawitAndGoliath/DNGGameModeBase.cpp
+++ Source/DawitAndGoliath/DNGGameModeBase.cpp
@@ -1,8 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DNGGameModeBase.h"
+#include "Engine.h"
 
 ADNGGameModeBase::ADNGGameModeBase()
 {
 	bUseSeamlessTravel = true;
+	BarrackCount = 2;
+	PlayerCount = 1;
 }
+
+void ADNGGameModeBase::GameSet()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Game Set");
+}
+
+void ADNGGameModeBase::OnBarrackDestroyed()
+{
+	--BarrackCount;
+	if (BarrackCount == 0) GameSet();
+}
+
+void ADNGGameModeBase::OnPlayerKilled()
+{
+	--PlayerCount;
+	if (PlayerCount == 0) GameSet();
+}
+
